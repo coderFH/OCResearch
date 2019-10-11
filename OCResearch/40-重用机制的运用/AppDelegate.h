24691a69b9ddd13839eee7ce0509570042815fb9
wangfuhao <wangfuhao@dongyin.net.cn>
2019-10-11 19:59:32 +0800

    重用机制的运用

---
+++ OCResearch/40-重用机制的运用/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  40-重用机制的运用
+//
+//  Created by wangfh on 2019/10/11.
+//  Copyright © 2019 wangfh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OCResearch/40-重用机制的运用/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  40-重用机制的运用
+//
+//  Created by wangfh on 2019/10/11.
+//  Copyright © 2019 wangfh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
