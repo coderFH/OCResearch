903337b527d03a8d224164138eafcae4432b5da6
wangfuhao <wangfuhao@dongyin.net.cn>
2019-10-16 18:36:07 +0800

    修改

--- OCResearch/03-OC对象-探究方法的调用/main.m
+++ OCResearch/03-OC对象-探究方法的调用/main.m
@@ -28,24 +28,21 @@
  class调用类方法的轨迹
  isa找meta-class，方法不存在，就通过superclass找父类
  */
+
 @interface FHPerson : NSObject
 - (void)run;
 @end
 
 @implementation FHPerson
-
 - (void)run {
     NSLog(@"%@我在跑",self);
 }
-
 @end
 
 @interface FHStudent : FHPerson
-
 @end
 
 @implementation FHStudent
-
 @end
 
 int main(int argc, const char * argv[]) {
@@ -54,9 +51,11 @@ int main(int argc, const char * argv[]) {
         //调用的顺序是:通过实例对象的isa找到类对象,如果类对象中没有run方法,会通过superClass指针,看看父类有没有该方法,有就调用,没有就会报错
         [stu run];
         
-        //这个调用顺序可以看到,NSObject分类中有个实例方法,
-        //FHStudent在调用test的时候,通过类对象的isa会找到元类对象,看是否存储有test方法,没有就会通过superClass找到父类看看是否有test方法,如果还没有
-        //会到根元素的元类中去找,如果还没有  此时会通过元类的superClass找到类对象,看是否有-号开头的test方法,有就会调用
+        //这个调用顺序可以看到,NSObject分类中有个实例方法- (void)test
+        //FHStudent在调用test的时候,通过类对象的isa会找到元类对象,看是否存储有test方法,
+        //没有就会通过superClass找到父类看看是否有test方法,如果还没有
+        //会到根元素的元类中去找,如果还没有
+        //此时会通过元类的superClass找到类对象,看是否有-号开头的test方法,有就会调用
         [FHStudent test];
     }
     return 0;
