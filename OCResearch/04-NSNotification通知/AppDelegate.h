0403ff336a6e275c40a702073606ffbf722ea80c
王福豪 <wangfuhao@dongyin.net>
2019-11-27 10:50:34 +0800

    补充

---
+++ OCResearch/04-NSNotification通知/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  04-NSNotification通知
+//
+//  Created by wangfh on 2019/11/27.
+//  Copyright © 2019 wangfh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OCResearch/04-NSNotification通知/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  04-NSNotification通知
+//
+//  Created by wangfh on 2019/11/27.
+//  Copyright © 2019 wangfh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
